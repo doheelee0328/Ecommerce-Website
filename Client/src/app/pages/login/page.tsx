afd76ef955d3d68c27eadd611316ad259ea27099
Dohee Lee <dojuhee2015@gmail.com>
2024-09-21 18:59:31 +0100

    add functionality to the login and register page

--- Client/src/app/pages/login/page.tsx
+++ Client/src/app/pages/login/page.tsx
@@ -1,7 +1,16 @@
+'use client'
+
+import { useState } from 'react'
+
 interface LoginProps {
   showText: boolean
   setShowText: () => void
   setRegister: () => void
+}
+
+type InputStates = {
+  email: string
+  password: string
 }
 
 export default function Login({
@@ -9,20 +18,48 @@ export default function Login({
   setShowText,
   setRegister,
 }: LoginProps) {
+  const inputInitialStates: InputStates = {
+    email: '',
+    password: '',
+  }
+
+  const [fields, setFields] = useState(inputInitialStates)
+
   const submitHandler = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
+    console.log('hello', fields)
+    setFields(inputInitialStates)
   }
+
+  const formChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    let value = e.target.value
+    setFields((prev: any) => ({
+      ...prev,
+      [e.target.name]: value,
+    }))
+  }
+
   return (
     <div>
       <form onSubmit={submitHandler}>
         <h1 className='text-black'>Welcome</h1>
         <label>
           <span>Email</span>
-          <input type='email' />
+          <input
+            type='email'
+            name='email'
+            onChange={formChangeHandler}
+            value={fields.email}
+          />
         </label>
         <label>
           <span>Password</span>
-          <input type={showText ? 'text' : 'password'} />
+          <input
+            type={showText ? 'text' : 'password'}
+            name='password'
+            onChange={formChangeHandler}
+            value={fields.password}
+          />
           <span onClick={setShowText}>Icon</span>
         </label>
         <button>SIGN IN</button>
